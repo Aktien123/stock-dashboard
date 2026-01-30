3e9b91adf5664b92deca56b35a855fd20ebff588
Aktien123 <immo_berg@herr-der-mails.de>
2026-01-30 21:00:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def create_line_chart(df, daily=None):
     ))
     fig.update_layout(
         height=300,
-        xaxis_title="Datum",
+        // xaxis_title="Datum",
         yaxis_title="Kurs EUR",
         margin=dict(l=10,r=10,t=30,b=10),
         plot_bgcolor="rgba(0,0,0,0)"
