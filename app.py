30bfbc992f6bf0cc2b6d5393a006071267b8a6fe
Aktien123 <immo_berg@herr-der-mails.de>
2026-01-31 11:16:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import plotly.graph_objects as go
 # --------------------------
 st.set_page_config(page_title="ETF & ETC Dashboard", layout="wide")
 
+padding-top: 0.25rem;
+
 # --------------------------
 # Skalierung auf 75% per CSS
 # --------------------------
